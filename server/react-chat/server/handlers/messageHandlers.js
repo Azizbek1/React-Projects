4303012c007486726f0abe066e0dc4eccb624168
Igor Agapov <aio350@yahoo.com>
2021-02-25 00:19:39 +0500

    update

--- server/react-chat/server/handlers/messageHandlers.js
+++ server/react-chat/server/handlers/messageHandlers.js
@@ -9,12 +9,14 @@ db.defaults({
   messages: [
     {
       messageId: '1',
+      userId: '1',
       senderName: 'Bob',
       messageText: 'What are you doing here?',
       createdAt: '2021-01-14'
     },
     {
       messageId: '2',
+      userId: '2',
       senderName: 'Alice',
       messageText: 'Go back to work!',
       createdAt: '2021-02-15'
